cb76de6bde4adc2fbf7ca1111a0d18e0e8c27809
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2023-11-02 15:57:15 -0700

    24.3 Complete

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,19 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float force;
+    private Rigidbody playerRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRB.AddForce(Vector3.forward * verticalInput * force);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    IEnumerator PortalCountdown()
+    {
+        yield return null;
     }
 }
