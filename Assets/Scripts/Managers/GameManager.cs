9fd174db1d145f4cfa2da4995914ada889e0511a
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2023-11-09 12:45:24 -0800

    Update GameManager.cs

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
     [Header("Player Metrics from GDD")]
     public float playerMass;
     public float playerDrag;
@@ -19,7 +21,14 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        gameManager = this.gameObject;
+        //This is a common approach to handling a class with a reference to itself.
+        //If instance variable doesn't exist, assign this object to it
+        if (Instance == null)
+            Instance = this;
+        //Otherwise, if the instance variable does exist, but it isn't this object, destroy this object.
+        //This is useful so that we cannot have more than one GameManager object in a scene at a time.
+        else if (Instance != this)
+            Destroy(this);
     }
 
     // Update is called once per frame
