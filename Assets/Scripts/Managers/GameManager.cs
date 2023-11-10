39e3e186679a071567878570955b6123b55ff11b
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2023-11-10 05:21:30 -0800

    Change Player to start inactive

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public Vector3 playerStartPos;
+    public Vector3 playerScale;
+    public float playerMass;
+    public float playerDrag;
+    public float playerPushForce;
+    public float playerMoveForce;
+    public float playerBounce;
+    public bool switchLevel { private get; set; }
+    private GameObject player;
+    private string nextLevel;
+
+    
+
+    // Awake is called before any Start methods are called
+    void Awake()
+    {
+        //This is a common approach to handling a class with a reference to itself.
+        //If instance variable doesn't exist, assign this object to it
+        if (Instance == null)
+            Instance = this;
+        //Otherwise, if the instance variable does exist, but it isn't this object, destroy this object.
+        //This is useful so that we cannot have more than one GameManager object in a scene at a time.
+        else if (Instance != this)
+            Destroy(this);
+    }
+
+    private void Start()
+    {
+        PlayerController[] inactivePlayer = GameObject.FindObjectsOfType<PlayerController>(true);
+        player = inactivePlayer[0].gameObject;
+
+        if(player != null)
+        {
+            player.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SwitchLevels()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player Controls/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    private SphereCollider playerCollider;
+    private Light powerupIndicator;
+    private Transform focalpoint;
+    private float moveForce;
+    public bool hasPowerup { get; private set; }
+
+    // OnEnable is called when the player is enabled
+    void OnEnable()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        playerCollider = GetComponent<SphereCollider>();
+        powerupIndicator = GetComponent<Light>();
+        focalpoint = GameObject.Find("Focal Point").transform;
+        transform.position = GameManager.Instance.playerStartPos;
+        transform.localScale = GameManager.Instance.playerScale;
+        playerRB.mass = GameManager.Instance.playerMass;
+        playerRB.drag = GameManager.Instance.playerDrag;
+        moveForce = GameManager.Instance.playerMoveForce;
+        playerCollider.material.bounciness = 0;
+        powerupIndicator.intensity = 0;        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRB.AddForce(focalpoint.forward.normalized * verticalInput * moveForce);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Startup"))
+        {
+            collision.gameObject.tag = "Ground";
+            playerCollider.material.bounciness = GameManager.Instance.playerBounce;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
